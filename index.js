f2a96955afc4eb979d4d522e9e32c4bc557ae288
westhof91 <88407013+westhof91@users.noreply.github.com>
2021-08-30 11:46:42 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name) {
+   return `Hi, my name is ${name}.`
+}
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+function introductionWithLanguageOptional(DeAndre, language= "JavaScript" ) {
+    return `Hi, my name is ${DeAndre} and I am learning to program in ${language}.`
+}
